bd117f10d251e9b8a74dc0d4375e8530ec055094
polinashelest <107417021+polinashelest@users.noreply.github.com>
2022-06-23 22:21:22 +0200

    Add files via upload

---
+++ Lab3.3/Program.cs
@@ -0,0 +1,41 @@
+﻿//Дана послідовність цілих чисел.
+//з неї всі додатні двозначні числа,
+//відсортувавши їх по зростанню
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = Enumerable
+                .Range(0, 200)
+                .OrderByDescending(x => x)
+                .ToArray();
+            Console.WriteLine("Input sequence: \n");
+            PrintSequence(sequence);
+            
+
+            var result = sequence
+                .Where(x => x >= 10 && x < 100)
+                .OrderBy(x => x)
+                .ToArray();
+            Console.WriteLine("Result sequence: \n");
+            PrintSequence(result);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintSequence(int[] sequence)
+        {
+            Console.WriteLine(string.Join(", ", sequence));
+            Console.WriteLine();
+        }
+    }
+}
